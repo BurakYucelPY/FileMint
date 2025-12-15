359f9be082f8796e25d65282241a230dc1603faa
karub360 <karub360@gmail.com>
2025-12-15 14:53:43 +0300

    Ana uygulama: Dosya formatı kontrolü ve yönlendirme sistemi eklendi

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import subprocess
+import os
 
-komut = [
-    r"C:\Program Files\LibreOffice\program\soffice.exe", 
-    '--headless',       
-    '--convert-to', 'pdf', 
-    'deneme.txt'        
-]
+from donusum import word_to_pdf, pdf_to_word
 
-print("Dönüştürme başladı")
-subprocess.run(komut)
-print("BİTTİ")
+def yonetici(dosya_ismi):
+
+    if not os.path.exists(dosya_ismi):
+        print("Dosya bulunamadı!")
+        return
+    uzanti = dosya_ismi.split('.')[-1].lower()
+    if uzanti in ['docx', 'doc']:
+        word_to_pdf.cevir(dosya_ismi)
+    
+    elif uzanti == 'pdf':
+        pdf_to_word.cevir(dosya_ismi)        
+    else:
+        print("⚠️ Bu dosya türünü tanıyan bir işçimiz yok.")
+
+# --- TEST ---
+print("--- SİSTEM BAŞLATILDI ---")
+
+yonetici("deneme.docx")
